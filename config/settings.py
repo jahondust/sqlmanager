574ae04728ee3f662c2dd6228d87e699c8bb59b6
Jahongir <jahondust@gmail.com>
2022-04-26 10:55:51 +0500

    fix cors

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,9 @@ ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "*"))
 
 CSRF_TRUSTED_ORIGINS = get_list(os.environ.get("CSRF_TRUSTED_ORIGINS", "http://127.0.0.1"))
 
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = get_list(os.environ.get("CORS_ALLOWED_ORIGINS", "http://127.0.0.1"))
 
 
